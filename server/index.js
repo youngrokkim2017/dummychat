9d750827bc594b03546ec6ffbfd7651f13acdc94
Patrick Kim <patrick.yrkim@gmail.com>
2022-11-08 23:55:33 -0800

    Add socket io connection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = 4000;
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.get('/', (req, res) => {
-    res.json({ data: 'hello world' })
+    // res.json({ data: 'hello world' })
+    res.sendFile(__dirname + '/index.html');
 })
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log('socket connection is ready')
+})
+
+httpServer.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`)
 })

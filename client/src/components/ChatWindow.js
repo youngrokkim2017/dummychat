f07a511553c62838b63e4534b5094ce026d6eaa6
Patrick Kim <patrick.yrkim@gmail.com>
2022-11-23 00:04:21 -0800

    Add file sharing functionality

--- client/src/components/ChatWindow.js
+++ client/src/components/ChatWindow.js
@@ -1,21 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
-import SendIcon from "@mui/icons-material/Send"
+import SendIcon from "@mui/icons-material/Send";
 import { Box, Typography, OutlinedInput, InputAdornment, IconButton, Card, InputLabel, Button } from "@mui/material";
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 // import { Textfield, Box, Container, Typography, OutlinedInput, InputAdornment, IconButton } from "@mui/material";
 // import Button from "@mui/material/Button";
 // import TextField from "@mui/material/TextField";
 // import Box from "@mui/material/Box";
-import { useOutletContext, useParams } from 'react-router-dom'
+import { useOutletContext, useParams } from 'react-router-dom';
 
 const ChatWindow = () => {
-  const { socket } = useOutletContext()
+  const { socket } = useOutletContext();
   // const [socket, setSocket] = useState(null)
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
   const [typing, setTyping] = useState(false);
-  const [typingTimeout, setTypingTimeout] = useState(null)
-  const { roomId } = useParams()
+  const [typingTimeout, setTypingTimeout] = useState(null);
+  const { roomId } = useParams();
+  const fileRef = useRef();
 
   // useEffect(() => {
   //   setSocket(io('http://localhost:4000'))
@@ -34,6 +36,10 @@ const ChatWindow = () => {
     socket.on('typing-stopped-from-server', () => {
       // console.log('typing...')
       setTyping(false)
+    }) 
+    socket.on('uploaded', (data) => {
+      // console.log(data)
+      setChat((prev) => [[...prev, { message: data.buffer, received: true, type: 'image' }]])
     }) 
   }, [socket])
 
@@ -64,6 +70,25 @@ const ChatWindow = () => {
     socket.emit('room-removed', { roomId })
   }
 
+  const selectFile = () => {
+    fileRef.current.click();
+  }
+
+  const handleFileSelected = () => {
+    if (!file) return
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      const data = reader.result
+      socket.emit('upload', { data, roomId })
+    }
+    setChat((prev) => [
+      ...prev,
+      { message: reader.result, received: false, type: 'image' }
+    ])
+  }
+
   return (
     <>
       <Card
@@ -81,8 +106,14 @@ const ChatWindow = () => {
         </Box>
         <Box sx={{ marginBottom: 5 }}>
           {chat.map((data, i) => (
-            // <Typography sx={{ textAlign: data.received ? 'left' : 'right' }} key={data.message}>{data.message}</Typography>
-            <Typography sx={{ textAlign: data.received ? 'left' : 'right' }} key={i}>{data.message}</Typography>
+            data.type === 'image' ? (
+              <img style={{ float: data.received ? 'left' : 'right' }} src={data.message} alt='image-message' width='100' />
+            )
+            :
+            (
+              // <Typography sx={{ textAlign: data.received ? 'left' : 'right' }} key={data.message}>{data.message}</Typography>
+              <Typography sx={{ textAlign: data.received ? 'left' : 'right' }} key={i}>{data.message}</Typography>
+            )
           ))}
         </Box>
         <Box component="form" onSubmit={handleForm}>
@@ -101,6 +132,10 @@ const ChatWindow = () => {
             onChange={handleInput}
             endAdornment={
               <InputAdornment position="end">
+                <input ref={fileRef} onChange={handleFileSelected} type="file" style={{ display: 'none' }} />
+                <IconButton type="button" edge="end" sx={{ marginRight: 1 }} onClick={selectFile} >
+                  <AttachFileIcon />
+                </IconButton>
                 <IconButton type="submit" edge="end">
                   <SendIcon />
                 </IconButton>
--- server/socket/sockets.js
+++ server/socket/sockets.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 import TypingController from './controllers/TypingController.js';
 import RoomController from './controllers/RoomController.js';
 import MessageController from './controllers/MessageController.js';
@@ -16,6 +18,12 @@ const sockets = (socket) => {
     socket.on('join-room', roomController.joinRoom)
     socket.on('new-room-created', roomController.newRoomCreated)
     socket.on('room-removed', roomController.roomRemoved)
+
+    socket.on('upload', ({ data, roomId }) => {
+        // const image = data
+        fs.writeFIle('upload/' + 'test.png', data, { encoding: 'base64' }, () => {})
+        socket.to(roomId).emit('uploaded', { buffer: data.toString('base64') })
+    })
 
     socket.on('disconnect', (socket) => {
         console.log('socket disconnected')
